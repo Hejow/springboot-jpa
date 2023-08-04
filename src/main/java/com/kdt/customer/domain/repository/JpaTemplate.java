dd2762b0f1526d2fe0d9ffc0020646ee947f7e0e
Hejow <gmlwh124@naver.com>
2023-08-05 04:23:22 +0900

    :sparkles: 순수 JPA용 템플릿 구현

---
+++ src/main/java/com/kdt/customer/domain/repository/JpaTemplate.java
@@ -0,0 +1,35 @@
+package com.kdt.customer.domain.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JpaTemplate {
+    private final EntityManagerFactory factory;
+
+    public JpaTemplate(EntityManagerFactory factory) {
+        this.factory = factory;
+    }
+
+    public <T> T executeInTransaction(EntityMangerCallback<T> callback) {
+        try (EntityManager entityManager = factory.createEntityManager()) {
+            return executeCallback(entityManager, callback);
+        }
+    }
+
+    private <T> T executeCallback(EntityManager entityManager, EntityMangerCallback<T> callback) {
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+            return callback.execute(entityManager);
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            transaction.commit();
+        }
+    }
+}
